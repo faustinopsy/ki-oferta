dd980ef7c6ef72a66b3aafb88e272bf4a2db34c7
faustinopsy <rodrigohipnose@gmail.com>
2026-09-03 18:32:13 +0000

    mapa de rotas

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,32 @@
 import { mapaderotas } from './rotas/rotas.js'
 import { navbar } from './navbar/navbar.js'
 
-const app = document.getElementById("app")
-navbar(mapaderotas)
-// #home
-let hash = location.hash || "#home"
-// console.log(mapaderotas)
-// console.log(mapaderotas[0])
-// console.log(mapaderotas[0].label)
-const roteador = {}
+const ROTA_PADRAO = '#home';
 
+const app = document.getElementById('app');
+const mapaDeRotas = criarMapaDeRotas(roteador);
+const paginaNaoEncontrada = {
+    pagina: (app) => { app.innerHTML = '<div>Página não encontrada 404</div>'; }
+};
 
-window.addEventListener("hashchange", ()=>{
-    mapaderotas[1].pagina(app)
-})
+function criarMapaDeRotas(rotas) {
+    const mapa = {};
+    for (const rota of rotas) {
+        mapa[rota.url] = rota;
+    }
+    return mapa;
+}
 
+async function renderizarRotaAtual() {
+    const hash = window.location.hash || ROTA_PADRAO;
+    const rota = mapaDeRotas[hash] || paginaNaoEncontrada;
+    await rota.pagina(app);
+}
+
+function iniciar() {
+    navbar(roteador);
+    renderizarRotaAtual();
+    window.addEventListener('hashchange', renderizarRotaAtual);
+}
+
+iniciar();
