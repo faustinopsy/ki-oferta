7738a797d5381e0b0d1906bb81bc0b573be88db6
faustinopsy <rodrigohipnose@gmail.com>
2026-09-03 19:36:06 +0000

    diminuindo a complexidade cognitiva do roteamento

--- src/js/main.js
+++ src/js/main.js
@@ -1,32 +1,19 @@
 import { mapaderotas } from './rotas/rotas.js'
 import { navbar } from './navbar/navbar.js'
 
-const ROTA_PADRAO = '#home';
-
 const app = document.getElementById('app');
-const mapaDeRotas = criarMapaDeRotas(roteador);
-const paginaNaoEncontrada = {
-    pagina: (app) => { app.innerHTML = '<div>Página não encontrada 404</div>'; }
-};
 
-function criarMapaDeRotas(rotas) {
-    const mapa = {};
-    for (const rota of rotas) {
-        mapa[rota.url] = rota;
-    }
-    return mapa;
+function renderizar() {
+  const hash = window.location.hash || '#home';
+  const rota = mapaderotas.find(r => r.url === hash);
+
+  if (rota) {
+    rota.pagina(app);
+  } else {
+    app.innerHTML = '<div>Página não encontrada 404</div>';
+  }
 }
 
-async function renderizarRotaAtual() {
-    const hash = window.location.hash || ROTA_PADRAO;
-    const rota = mapaDeRotas[hash] || paginaNaoEncontrada;
-    await rota.pagina(app);
-}
-
-function iniciar() {
-    navbar(roteador);
-    renderizarRotaAtual();
-    window.addEventListener('hashchange', renderizarRotaAtual);
-}
-
-iniciar();
+navbar(mapaderotas);
+renderizar();
+window.addEventListener('hashchange', renderizar);
